6c57fbcba0a287c6c40119f71e5224bfb59e0ddd
Luffy Web <drstrangexr1@gmail.com>
2024-08-17 21:55:02 +0600

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 29452145
+API_HASH = 5a2784e571fe5043852d32396a34a13b
+BOT_TOKEN = 6340500779:AAGJcz-AorhwZnDKGZI84ehcoiM1LI0LxSE
+SESSION = BQHBZ3EAKuTw7gsiKOjyd7m5B_9ZCnwDbCgFfrkemGudO43TMbo5PYNpMlTRuJ9js_CV5LuXgZvhrqLkrvqjGro9yKG2dwUtcRXxjsyXhvYpH0KNWqKMoCTRVoaqxac8eq9kZJrMxkwEabkCPcWPi-qVtp52V9hBul8D8SV1Zgj0rHTYoJFh_ZQAXbYvWaYmSWYe8IX4oafWP2WwK-MThTbioDyThRsQysVj2nSSFKTMaVyv2IihKm1XGtSu27hcxVE8EHbqxGGUatPDLaTq4CV9ydf68cnWFXOiFPH7BfThZxjfJJyEu2rpsCThaG16VqeVIjvufanXGbaHBUldW4g99WJhbgAAAAGGD3onAA
+FORCESUB = Luffywebbd
+AUTH = 6544128551
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
